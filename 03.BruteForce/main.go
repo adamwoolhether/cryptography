f4f2c5ccf8b15686c0b49a3ec5bc0ab905b2f522
adamwoolhether <adamwoolhether@gmail.com>
2023-01-25 17:26:45 +0800

    brute force

--- 03.BruteForce/main.go
+++ 03.BruteForce/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"math"
 )
+
+func main() {
+	fmt.Println(alphabetSize(4))
+}
 
 /*
 The number of total characters in an alphabet depends on how many bits each character in an encoded string can represent.
@@ -15,12 +21,49 @@ The number of total characters in an alphabet depends on how many bits each char
 8 bits -> 256 chars
 */
 
-func main() {
-	fmt.Println(alphabetSize(4))
-}
-
 // alphabetSize takes returns the size of alphabet that can be represented
 // based on the number of bits per a given character.
 func alphabetSize(numBits int) float64 {
 	return math.Pow(2, float64(numBits))
+}
+
+/*
+A brute-force attack is simple: the attacker systematically guesses every possible combination.
+ A 256-bit key is not twice as hard to guess as a 128-bit key, it is 2^128 times harder to guess!
+*/
+
+// findKey will attempt to brute force a given message, returning
+// the key used to encrypt the password if found.
+func findKey(encrypted []byte, decrypted string) ([]byte, error) {
+	for i := 0; i < int(math.Pow(2, 24)); i++ {
+		b := intToBytes(i)
+
+		if decrypt := crypt(encrypted, b); string(decrypt) == decrypted {
+			return b, nil
+		}
+	}
+
+	return nil, fmt.Errorf("unable to decrypt")
+}
+
+func crypt(dat, key []byte) []byte {
+	final := []byte{}
+	for i, d := range dat {
+		final = append(final, d^key[i])
+	}
+	return final
+}
+
+func intToBytes(num int) []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, int64(num))
+	if err != nil {
+		fmt.Println("Error in intToBytes:", err)
+		return nil
+	}
+	bs := buf.Bytes()
+	if len(bs) > 3 {
+		return bs[:3]
+	}
+	return bs
 }
